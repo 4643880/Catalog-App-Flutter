f7f71eaf3dac50b75058b174d408456147bb7f05
Aizaz Haider <aizazisonline@gmail.com>
2021-09-02 19:05:25 +0500

    Extracted Theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog_app/pages/home_page.dart';
 import 'package:flutter_catalog_app/pages/login_page.dart';
 import 'package:flutter_catalog_app/utils/routes.dart';
+import 'package:flutter_catalog_app/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -20,11 +21,11 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: GoogleFonts.lato().fontFamily,
-        colorScheme: ColorScheme.light(),
-      ),
-      //home: HomePage(),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      
+
+
       initialRoute: MyRoutes.home_route,
       routes: {
         "/": (context) => LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
+        //backgroundColor: Colors.deepPurple,
         title: Center(
           child: Text(
             "Catalog App",
